c86876914b04d77246db92646981626f3ca8586e
Jory Smith <46983170+JorySmith@users.noreply.github.com>
2022-03-04 13:37:44 -0600

    Updated App and Sidebar

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -3,7 +3,7 @@ import './Avatar.css'
 export default function Avatar({ src }) {
   return (
     <div className='avatar'>
-      <img src={src} alt="user profile picture" />
+      <img src={src} alt="user profile avatar" />
     </div>
   )
 }
