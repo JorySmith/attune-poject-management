ed28d0bf5fd515fa971415a077dc5ff1080f818b
Jory Smith <46983170+JorySmith@users.noreply.github.com>
2022-03-04 08:49:41 -0600

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+// Styles
 import './App.css'
+
+// Pages/Components
+import Dashboard from './pages/Dashboard/Dashboard'
+import Create from './pages/Create/Create'
+import Login from './pages/Login/Login'
+import Signup from './pages/Signup/Signup'
+import Project from './pages/Project/Project'
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <div className="container">
+          <Switch>
+            {/* force exact route match, 'exact' attribute for route */}
+            <Route exact path='/'>
+              <Dashboard />
+            </Route>
+            <Route path='/create'>
+              <Create />
+            </Route>
+            <Route path='/project/:id'>
+              <Project />
+            </Route>
+            <Route path='/login'>
+              <Login />
+            </Route>
+            <Route path='/signup'>
+              <Signup />
+            </Route>        
+          </Switch>
+        </div>        
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+// Styles/Images
+import "./Navbar.css";
+import Temple from "../assets/temple.svg";
+
+export default function Navbar() {
+  return (
+    <div>
+      <ul>
+        <li className="logo">
+          <img src={Temple} alt="attune logo" />
+          <span>Attune</span>
+        </li>
+        <li>
+          <Link to='/login'>Login</Link>
+        </li>
+        <li>
+          <Link to='/signup'>Signup</Link>
+        </li>
+        <li>
+          <button className="btn">Logout</button>
+        </li>
+      </ul>
+    </div>
+  );
+}
