a616cb72fd834f50979ed9ad6d3cafa958232235
Jory Smith <46983170+JorySmith@users.noreply.github.com>
2022-03-07 13:03:29 -0600

    Updated ProjectFilter

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import { useCollection } from '../../hooks/useCollection'
 import ProjectList from '../../components/ProjectList'
+import ProjectFilter from './ProjectFilter'
 
 // Styles
 import './Dashboard.css'
@@ -13,6 +14,7 @@ export default function Dashboard() {
     <div>
       <h2 className='page-title'>Dashboard</h2>
       {error && <p className='error'>{error}</p>}
+      {documents && <ProjectFilter />}
       {documents && <ProjectList projects={documents} />}
     </div>
   )
--- src/pages/Dashboard/ProjectFilter.js
+++ src/pages/Dashboard/ProjectFilter.js
@@ -1,12 +1,28 @@
-
+import { useState } from "react"
 
 const filterList = ['all', 'mine', 'development', 'design', 'marketing', 'sales']
 
 export default function ProjectFilter() {
+  const [filterOption, setFilterOption] = useState('all')
+
+  const handleClick = (option) => {
+    setFilterOption(option)
+    console.log(option)
+  }
+
   return (
     <div className="project-filter">
       <nav>
-
+        <p>Filter Projects By:</p>
+        {/* Display filter options */}
+        {filterList.map(option => (
+          <button 
+            key={option} 
+            className={filterOption === option ? 'active' : ''}
+            onClick={() => handleClick(option)}>
+              {option}
+          </button>
+        ))}
       </nav>
     </div>
   )
